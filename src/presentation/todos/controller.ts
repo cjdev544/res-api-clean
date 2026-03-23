f9f2066e90f3c092d3a2f8c8af01a06e8bd253b0
CjDev544 <cjdev544@example.com>
2026-03-23 20:05:55 +0100

    Removed postgres_data from git

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,12 +1,104 @@
 import { Request, Response } from 'express'
+import { prisma } from '../../data/postgres'
+import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos'
 
 export class TodoController {
-  public getTodos(_req: Request, res: Response) {
-    console.log('todoController')
-    return res.status(200).json([
-      { id: 1, text: 'buy  milk', createdAt: Date.now() },
-      { id: 2, text: 'buy  bread', createdAt: null },
-      { id: 3, text: 'buy  butter', createdAt: Date.now() },
-    ])
+  public getTodos = async (_req: Request, res: Response) => {
+    try {
+      const todos = await prisma.todo.findMany()
+      return res.status(200).json(todos)
+    } catch (error) {
+      res.status(500).json({
+        error: 'Error server',
+      })
+    }
+  }
+
+  public getTodoById = async (req: Request, res: Response) => {
+    const id = +req.params.id
+    if (isNaN(id))
+      return res.status(400).json({ error: 'ID argument is not a number' })
+
+    try {
+      const todo = await prisma.todo.findFirst({
+        where: { id },
+      })
+
+      todo
+        ? res.status(200).json(todo)
+        : res.status(404).json({ error: `TODO with id: ${id} not found` })
+    } catch (error) {
+      res.status(500).json({
+        error: 'Error server',
+      })
+    }
+  }
+
+  public createTodo = async (req: Request, res: Response) => {
+    const [error, createTodoDto] = CreateTodoDto.create(req.body)
+    if (error) return res.status(400).json({ error })
+
+    try {
+      const todo = await prisma.todo.create({
+        data: createTodoDto!,
+      })
+
+      res.json(todo)
+    } catch (error) {
+      res.status(500).json({
+        error: 'Error server',
+      })
+    }
+  }
+
+  public updateTodo = async (req: Request, res: Response) => {
+    const id = +req.params.id
+    const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id })
+    if (error) return res.status(400).json({ error })
+
+    try {
+      const todo = await prisma.todo.findFirst({
+        where: { id },
+      })
+
+      if (!todo)
+        return res.status(404).json({ error: `Todo with id ${id} not found` })
+
+      const updatedTodo = await prisma.todo.update({
+        where: { id },
+        data: updateTodoDto!.values,
+      })
+
+      res.json(updatedTodo)
+    } catch (error) {
+      res.status(500).json({
+        error: 'Error server',
+      })
+    }
+  }
+
+  public deleteTodo = async (req: Request, res: Response) => {
+    const id = +req.params.id
+
+    const todo = await prisma.todo.findFirst({
+      where: { id },
+    })
+
+    if (!todo)
+      return res.status(404).json({ error: `Todo with id ${id} not found` })
+
+    try {
+      const deleted = await prisma.todo.delete({
+        where: { id },
+      })
+
+      deleted
+        ? res.json(deleted)
+        : res.status(400).json({ error: `Todo with id ${id} not found` })
+    } catch (error) {
+      res.status(500).json({
+        error: 'Error server',
+      })
+    }
   }
 }
